a2e61abe29cf2de2db65664fa33d8cdfc56689f7
Bart Agapinan <bart@sonic.net>
2007-11-26 00:55:54 +0000

    Changing main page

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ ActionController::Routing::Routes.draw do |map|
   # instead of a file named 'wsdl'
   map.connect ':controller/service.wsdl', :action => 'wsdl'
 
-  map.connect '', :controller => 'main'
+  map.connect '', :controller => 'main', :action => 'index'
 
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id'
