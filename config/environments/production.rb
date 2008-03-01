253f9b3c3cd5291d30721fa5e4897174248b12e2
Bart Agapinan <bart@sonic.net>
2008-03-01 06:52:41 +0000

    Fixing mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,14 +19,14 @@ config.action_controller.perform_caching             = true
 # Disable delivery errors if you bad email addresses should just be ignored
 # config.action_mailer.raise_delivery_errors = false
 
-ActionMailer::Base.server_settings = { 
+ActionMailer::Base.smtp_settings = { 
 :address => "mail.travellerbook.com", 
 :port => 465, 
 :domain => "travellerbook.com", 
 :authentication => :login, 
 :user_name => "do-not-reply+travellerbook.com", 
 :password => "/1<$xFMp!-k-", 
-} 
+}
 
 # Person id of the "Nobody" user, where given away books stay
 NOBODY_USER = 2
