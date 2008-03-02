1095d16bbaba2a09193b1ec2e3981bda26dd56f1
Bart Agapinan <bart@sonic.net>
2008-03-02 00:51:05 +0000

    Removing rmagick junk

--- vendor/rmagick-2.2.2/doc/ex/bilevel_channel.rb
+++
@@ -1,9 +0,0 @@
-#!/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/bin/ruby -w
-
-require 'RMagick'
-
-img = Magick::Image.read('images/Flower_Hat.jpg').first
-result = img.bilevel_channel(2*Magick::QuantumRange/3, Magick::RedChannel)
-result.write('bilevel_channel.jpg')
-exit
-
--- vendor/rmagick-2.2.2/doc/ex/raise.rb
+++
@@ -1,8 +0,0 @@
-#!/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/bin/ruby -w
-require 'RMagick'
-
-# Demonstrate the Image#raise method.
-img = Magick::Image.read('images/Flower_Hat.jpg').first
-img = img.raise
-img.write('raise.jpg')
-exit
